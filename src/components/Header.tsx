baf5e3b6668ed12d018b9eb453fd4342236d7646
lusousouza <luca.s.ouza@hotmail.com>
2022-07-27 12:57:37 -0300

    components do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Users from "../assets/users.svg";
+import Plataformas from "../assets/plataformas.svg";
 
 const Header = () => {
   return (
     <div className="bg-[#090909] ">
-      <div className=" w-full bg-MaskgroupFallGuys h-[770px] bg-no-repeat bg-cover">
+      <div className=" flex flex-col items-center bg-MaskgroupFallGuys h-[770px] bg-no-repeat bg-cover">
         <Navbar />
-        <div className="flex flex-col  w-[580px] ml-[14rem] mt-[4rem]">
+        <div className="flex flex-col  w-[580px] h-full  my-[4rem]">
           <div className="font-['Poppins700'] font-bold">
             <h1 className=" text-[#D2A300] text-bv">Bem Vindo ao</h1>
             <h1 className="text-fgu text-white">Fall Guys : Ultimate!</h1>
@@ -19,6 +21,17 @@ const Header = () => {
             elementum girarzis, nisi eros vermeio.Atirei o pau no gatis, per
             gatis num morreus.
           </p>
+          <button className="w-[340px] h-[70px] bg-[#D72798] my-8 rounded-xl">
+            Saiba Mais
+          </button>
+          <div className=" flex ">
+            <img src={Users} alt="users" />
+            <p className="m-2 text-white">+ 20 milhões de jogadores</p>
+          </div>
+          <div className=" text-white mt-24">
+            <p>Integração com as maiores plataformas do mundo</p>
+            <img src={Plataformas} alt="" />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import Logo from "../assets/LogoFallGuys.svg";
 
 const Navbar = () => {
   return (
     <div className=" border-b border-white/[.20] w-full">
-      <div className="flex justify-around items-center ">
+      <div className="flex justify-evenly  items-center ">
         <img src={Logo} alt="" />
-        <div className=" flex items-center justify-evenly w-1/4 mr-10 text-white">
+        <div className=" flex items-center justify-evenly w-1/4  text-white">
           <a href="">Sobre</a>
           <a href="">Download</a>
         </div>
